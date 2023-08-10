9fc8de05cfd5980353cccc1b7d4dd6161cefb251
Mauricio Dolacio <mauriciodolacio@Outlook.com>
2023-08-09 23:51:20 -0300

    Operações compostas

--- KotlinCurso2/src/main/kotlin/Main.kt
+++ KotlinCurso2/src/main/kotlin/Main.kt
@@ -8,20 +8,37 @@ fun main() {
 
     val aumento = "1.1".toBigDecimal()
     val salariosComAumento: Array<BigDecimal> = salarios
-        .map { salario ->
-            if(salario < "5000".toBigDecimal()){
-                salario + "500".toBigDecimal()
-            } else {
-                (salario * aumento).setScale(2, RoundingMode.UP)
-            }
-        }
+        .map { salario -> calculaAumentoRelativo(salario, aumento) }
         .toTypedArray()
 
     println(salariosComAumento.contentToString())
+
+    val gastoInicial = salariosComAumento.somatoria()
+    println(gastoInicial)
+
+    val meses = 6.toBigDecimal()
+    val gastoTotal = salariosComAumento.fold(gastoInicial) { acumulator, salario ->
+        acumulator + (salario * meses).setScale(2, RoundingMode.UP)
+    }
+    println(gastoTotal)
 }
+
+private fun calculaAumentoRelativo(salario: BigDecimal, aumento: BigDecimal)
+: BigDecimal =
+    if (salario < "5000".toBigDecimal()) {
+        salario + "500".toBigDecimal()
+    } else {
+        (salario * aumento).setScale(2, RoundingMode.UP)
+    }
 
 fun bigDecimalArrayOf(vararg valores: String): Array<BigDecimal> {
     return Array<BigDecimal>(valores.size) { i ->
         valores[i].toBigDecimal()
+    }
+}
+
+fun Array<BigDecimal>.somatoria(): BigDecimal {
+    return this.reduce { acumulator, valor ->
+        acumulator + valor
     }
 }
