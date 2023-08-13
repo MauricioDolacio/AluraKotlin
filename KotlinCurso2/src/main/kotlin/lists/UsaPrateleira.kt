567f267415569c9ddc260dcc1995b916d1069a35
Mauricio Dolacio <mauriciodolacio@Outlook.com>
2023-08-13 00:26:24 -0300

    Aplicação de listas imutaveis

---
+++ KotlinCurso2/src/main/kotlin/lists/UsaPrateleira.kt
@@ -0,0 +1,11 @@
+package main.kotlin.lists
+
+fun main() {
+    val prateleira = Prateleira(genero = "Literatura", livros = listaDeLivros)
+
+    val porAutor = prateleira.organizarPorAutor()
+    val porAnoPublicacao = prateleira.organizarPorAnoPublicacao()
+
+    porAutor.imprimeComMarcadores()
+    porAnoPublicacao.imprimeComMarcadores()
+}
